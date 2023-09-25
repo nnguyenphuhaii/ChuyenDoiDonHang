462f1ee14293bcc3d3f6bd19233f312853ddb006
Zeiko <nnnnguyenphuhaiiii@gmail.com>
2023-09-25 23:49:03 +0700

    Làm Export

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,89 +1,89 @@
 ﻿namespace ChuyenDoiDonHang
 {
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            btnImport = new Button();
-            btnRun = new Button();
-            lblResult = new Label();
-            SuspendLayout();
-            // 
-            // btnImport
-            // 
-            btnImport.Location = new Point(40, 121);
-            btnImport.Name = "btnImport";
-            btnImport.Size = new Size(75, 23);
-            btnImport.TabIndex = 0;
-            btnImport.Text = "Import";
-            btnImport.UseVisualStyleBackColor = true;
-            btnImport.Click += btnImport_Click;
-            // 
-            // btnRun
-            // 
-            btnRun.Location = new Point(167, 121);
-            btnRun.Name = "btnRun";
-            btnRun.Size = new Size(75, 23);
-            btnRun.TabIndex = 1;
-            btnRun.Text = "Run";
-            btnRun.UseVisualStyleBackColor = true;
-            btnRun.Click += btnRun_Click;
-            // 
-            // lblResult
-            // 
-            lblResult.AutoSize = true;
-            lblResult.Location = new Point(12, 9);
-            lblResult.Name = "lblResult";
-            lblResult.Size = new Size(45, 15);
-            lblResult.TabIndex = 2;
-            lblResult.Text = "Result: ";
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.Window;
-            ClientSize = new Size(284, 261);
-            Controls.Add(lblResult);
-            Controls.Add(btnRun);
-            Controls.Add(btnImport);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
-            Name = "Form1";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "HomeForm";
-            Load += Form1_Load;
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			btnImport = new Button();
+			btnRun = new Button();
+			lblResult = new Label();
+			SuspendLayout();
+			// 
+			// btnImport
+			// 
+			btnImport.Location = new Point(12, 106);
+			btnImport.Name = "btnImport";
+			btnImport.Size = new Size(75, 23);
+			btnImport.TabIndex = 0;
+			btnImport.Text = "Import";
+			btnImport.UseVisualStyleBackColor = true;
+			btnImport.Click += btnImport_Click;
+			// 
+			// btnRun
+			// 
+			btnRun.Location = new Point(97, 106);
+			btnRun.Name = "btnRun";
+			btnRun.Size = new Size(75, 23);
+			btnRun.TabIndex = 1;
+			btnRun.Text = "Run";
+			btnRun.UseVisualStyleBackColor = true;
+			btnRun.Click += btnRun_Click;
+			// 
+			// lblResult
+			// 
+			lblResult.AutoSize = true;
+			lblResult.Location = new Point(12, 9);
+			lblResult.Name = "lblResult";
+			lblResult.Size = new Size(45, 15);
+			lblResult.TabIndex = 2;
+			lblResult.Text = "Result: ";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = SystemColors.Window;
+			ClientSize = new Size(184, 161);
+			Controls.Add(lblResult);
+			Controls.Add(btnRun);
+			Controls.Add(btnImport);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			Name = "Form1";
+			StartPosition = FormStartPosition.CenterScreen;
+			Text = "HomeForm";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private Button btnImport;
-        private Button btnRun;
-        private Label lblResult;
-    }
+		private Button btnImport;
+		private Button btnRun;
+		private Label lblResult;
+	}
 }
--- Form1.cs
+++ Form1.cs
@@ -7,74 +7,85 @@ using System.Windows.Forms;
 
 namespace ChuyenDoiDonHang
 {
-    public partial class Form1 : Form
-    {
-        //string apiKey = "EZAKb9716aa6afc3430781d263bc5a21aed5h8RCmah8QB2Z5HEzQwiu2Q";
-        //string apiKey = Environment.GetEnvironmentVariable("EZAKb9716aa6afc3430781d263bc5a21aed5h8RCmah8QB2Z5HEzQwiu2Q")!;
-        //string apiUrl = "https://api.easypost.com/v2/verify/address";
+	public partial class Form1 : Form
+	{
+		//string apiKey = "EZAKb9716aa6afc3430781d263bc5a21aed5h8RCmah8QB2Z5HEzQwiu2Q";
+		//string apiKey = Environment.GetEnvironmentVariable("EZAKb9716aa6afc3430781d263bc5a21aed5h8RCmah8QB2Z5HEzQwiu2Q")!;
+		//string apiUrl = "https://api.easypost.com/v2/verify/address";
 
-        //string street1 = "821 Old Paint Road";
-        //string street2 = "";
-        //string city = "Raymore";
-        //string state = "Missouri";
-        //string zip = "64083";
-        //string country = "United States";
+		//string street1 = "821 Old Paint Road";
+		//string street2 = "";
+		//string city = "Raymore";
+		//string state = "Missouri";
+		//string zip = "64083";
+		//string country = "United States";
+		List<Order> orders = new List<Order>();
 
 
-        public Form1()
-        {
-            InitializeComponent();
-        }
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+		private void Form1_Load(object sender, EventArgs e)
+		{
 
-        }
+		}
 
-        private void btnImport_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Chọn tệp tin CSV
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-                openFileDialog.Filter = "CSV Files|*.csv";
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    string filePath = openFileDialog.FileName;
+		private void btnImport_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// Chọn tệp tin CSV
+				OpenFileDialog openFileDialog = new OpenFileDialog();
+				openFileDialog.Filter = "CSV Files|*.csv";
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					string filePath = openFileDialog.FileName;
 
-                    // Đọc dữ liệu từ file CSV
-                    List<Order> orders = new List<Order>();
-                    using (var reader = new StreamReader(filePath))
-                    {
-                        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-                        {
-                            HasHeaderRecord = true // Không sử dụng dòng đầu tiên làm tiêu đề
-                        };
-                        using (var csv = new CsvReader(reader, config))
-                        {
-                            orders = csv.GetRecords<Order>().ToList();
-                        }
-                    }
-                    Console.WriteLine(orders);
-                    // Hiển thị thông báo
-                    //StringBuilder message = new StringBuilder();
-                    //foreach (var order in orders)
-                    //{
-                    //    message.AppendLine($"Shop Domain: {order.SourceAnalytics}, Name: {order.Name}, Shipping Method: {order.ShippingMethod}, Created At: {order.CreatedAt}");
-                    //    // (Thêm các thuộc tính khác vào message tùy theo nhu cầu)
-                    //}
+					// Đọc dữ liệu từ file CSV
+					orders.Clear();
+					using (var reader = new StreamReader(filePath))
+					{
+						var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+						{
+							HasHeaderRecord = false // Không sử dụng dòng đầu tiên làm tiêu đề
+						};
+						using (var csv = new CsvReader(reader, config))
+						{
+							orders = csv.GetRecords<Order>().ToList();
+							orders.RemoveAt(0);
+						}
+					}
+					
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
 
-                    //MessageBox.Show(message.ToString(), "Thông tin đơn hàng", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+		private void btnRun_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+			saveFileDialog1.Filter = "CSV Files|*.csv";
+			saveFileDialog1.Title = "Save CSV File";
+			saveFileDialog1.ShowDialog();
 
-        private void btnRun_Click(object sender, EventArgs e)
-        {
-        }
-    }
+			if (saveFileDialog1.FileName != "")
+			{
+				ExportToCsv(orders, saveFileDialog1.FileName);
+				MessageBox.Show("File đã được lưu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+		public void ExportToCsv(List<Order> orders, string filePath)
+		{
+			using (var writer = new StreamWriter(filePath))
+			using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+			{
+				csv.WriteRecords(orders);
+			}
+		}
+	}
 }
